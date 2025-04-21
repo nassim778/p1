3022a65be59186f89e2d9c03fd5ebaaa81736f47
nassim778 <nassim.rached777@gmail.com>
2025-04-21 21:00:55 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Al Assala SHOP</title>
+    <link rel="stylesheet" href="style.css" />
+    
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>Contact Us !</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="shop.php">Shop</a></li> 
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                    <li><a href="about.html">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+       
+<div class="contact" style="text-align: center;">
+    <h2>
+        <ul>
+                    <li><a >EMAIL:alassala@gmail.com</a></li>
+                    <li><a >TEL:99888777</a></li> 
+                    <li><a >TEL:66555444</a></li>
+    </ul>
+</h2>
+</div>
+<div style="text-align: center; margin: 20px 0;">
+     
+     <h2>Our Location : </h2>
+  <iframe
+    width="400"
+    height="300"
+    frameborder="0"
+    scrolling="no"
+    marginheight="0"
+    marginwidth="0"
+    src="https://www.openstreetmap.org/export/embed.html?bbox=8.993701%2C33.426742%2C9.033701%2C33.486742&layer=mapnik&marker=33.456742%2C9.013701"
+    style="border: 1px solid #ddd; display: inline-block;">
+  </iframe>
+  <br/>
+  <small style="display: inline-block; margin-top: 5px;">
+    <a href="https://www.openstreetmap.org/?mlat=33.456742&mlon=9.013701#map=15/33.456742/9.013701" target="_blank">
+      View Larger Map
+    </a>
+  </small>
+</div>
+    </main>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2025 Al Assala. All Rights Reserved.</p>
+        </div>
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
